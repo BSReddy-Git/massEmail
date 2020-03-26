f2013bea621eb07bf3bf72ed4a8fad47dbdb726d
8977shiva <b.skreddy1997@gmail.com>
2020-03-26 21:48:51 +0530

    mongo added

---
+++ mongo.js
@@ -0,0 +1,41 @@
+var mongoose = require("mongoose");
+ const  Schema = mongoose.Schema({
+    _id: Object
+});
+ 
+
+
+ const mongo_connections ={
+    dev: {
+      "mongo_host": "mongo.dev.talentscreen.io"
+    },
+    "qa": {
+      "mongo_host": "mongo.qa.talentscreen.io"
+    },
+    "stag": {
+      "mongo_host": "mongo.stag.talentscreen.io"
+    },
+    "prod": {
+      "mongo_host": "mongo.echo.talentscreen.io" //working 
+    },
+    "mongo_port": "27017",
+    "user": "ipkumar",
+    "password": "kumarip131"
+  }
+// console.log( mongo_connections.prod);
+
+ const db=mongoose.createConnection(
+  "mongodb://" +
+    mongo_connections.user +
+    ":" +
+    mongo_connections.password +
+    "@" +
+    mongo_connections.prod.mongo_host +
+    "/" +'talent_ops', { useNewUrlParser: true } ,
+    ()=>{
+        console.log('connected')
+    }
+    
+);
+//  const domain = db.model('distributionInvalidDomains', Schema, 'distributionInvalidDomains')
+//  console .log (domain)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+mongoose.createConnection(
+//   "mongodb://" +
+//     mongo_connections.user +
+//     ":" +
+//     config.mongo_connections.password +
+//     "@" +
+//     config.mongo_connections.dev.mongo_host +
+//     "/" +'talent_ops', { useNewUrlParser: true } ,
+//     ()=>{
+//         console.log('connected')
+//     }
+    
+// );
